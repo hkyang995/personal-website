547facd65208410d75c0e3a760b0efc3cec1c2b8
Henry Yang <hkyang@csu.fullerton.edu>
2020-02-03 12:42:16 -0800

    fix wide button

--- src/components/header/styledbtn.js
+++ src/components/header/styledbtn.js
@@ -11,13 +11,13 @@ const BtnWrapper = styled.div`
 const BtnOutline = styled.a`
   text-decoration: none;
   margin: 30px auto 0;
-  width: 210px;
+  width: ${props => (props.wide ? '275px' : '210px')};
   height: 40px;
   display: block;
 `
 const BtnShape = styled.rect`
-  stroke-dasharray: 100 310;
-  stroke-dashoffset: -304;
+  stroke-dasharray: ${props => (props.wide ? '120 450' : '100 310')};
+  stroke-dashoffset: ${props => (props.wide ? '-385' : '-304')};
   stroke-width: 5px;
   fill: transparent;
   stroke: #fff;
@@ -28,7 +28,7 @@ const BtnShape = styled.rect`
   ${BtnOutline}:hover & {
     stroke-width: 2px;
     stroke-dashoffset: 0;
-    stroke-dasharray: 550;
+    stroke-dasharray: 650;
   }
 `
 const BtnText = styled.div`
@@ -36,8 +36,8 @@ const BtnText = styled.div`
   position: relative;
   color: #fff;
   font-size: 1.5em;
-  letter-spacing: ${props => (props.wide ? '1px' : '4px')};
-  margin-right: ${props => (props.wide ? '1px' : '-4px')};
+  letter-spacing: 4px;
+  margin-right: -4px;
 `
 
 const ButtonIcon = styled.b`
@@ -46,11 +46,15 @@ const ButtonIcon = styled.b`
 
 export const StyledBtn = props => (
   <BtnWrapper>
-    <BtnOutline href={props.href} target="_blank">
-      <svg height="40" width="210">
-        <BtnShape height="40" width="210" />
+    <BtnOutline wide={props.wide} href={props.href} target="_blank">
+      <svg height="40" width={props.wide ? '275' : '210'}>
+        <BtnShape
+          height="40"
+          wide={props.wide}
+          width={props.wide ? '275' : '210'}
+        />
       </svg>
-      <BtnText wide={props.wide}>
+      <BtnText>
         <ButtonIcon>{props.icon}</ButtonIcon> {props.text}
       </BtnText>
     </BtnOutline>
